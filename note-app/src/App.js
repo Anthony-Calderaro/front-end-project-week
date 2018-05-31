869873f3df26e366997d3b161088cf01aa0b83f2
Anthony <anthony@anthonys-mbp.lan>
2018-05-31 16:10:39 -0400

    added axios connections to heroku

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -3,7 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import  notes  from './notes.js';
+import notes from './notes.js';
+import axios from 'axios';
 
 // Combine Component Imports into a single file at end
 import Header from './components/header/header.js';
@@ -16,32 +17,60 @@ import Home from './components/home/home.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { notes }  
+    this.state = { notes };
   }
 
-  // componentDidMount() {
+  componentDidMount() {
+    axios
+      .get('https://blooming-bastion-41361.herokuapp.com/api/notes')
+      .then(response => {
+        this.setState({ notes: response.data })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  };
 
-  //   axios
-  //     .get('http://localhost:5000/notes')
-  //     .then(response => {
-  //       this.setState({ notes: response.data })
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }
+  handleUpdate = (id, update) => {
+    axios
+      .put(`https://blooming-bastion-41361.herokuapp.com/api/notes/${id}`, update)
+      .then(response => {
+        let { notes } = this.state;
+        notes.map(((note) => {
+          if (note._id === response.data._id) {
+            note.title = response.data.title;
+            note.content = response.data.content;
+            return note
+          }
+          return note
+        });
+      this.setState({ notes: notes });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  deleteNote = (noteId) => {
+    axios.delete(`${url}/${noteId}`)
+      .then(response => {
+        this.newRequest();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div className="App">
 
-        <Route 
-        exact path='/' title='List View' component={Header} />
-        <Route exact path='/' render={props => <Home notes={this.state.notes}/>} />
+        <Route
+          exact path='/' title='List View' component={Header} />
+        <Route exact path='/' render={props => <Home notes={this.state.notes} />} />
 
 
         <Route exact path='/new' component={Header} />
-        <Route exact path='/new' render={props => <InputForm notes={this.state.notes}/>} />
+        <Route exact path='/new' render={props => <InputForm notes={this.state.notes} />} />
 
 
         <Route exact path='/note/:id' component={Header} />
